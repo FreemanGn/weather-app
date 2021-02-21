6037176a405d7cd07b26a9d0a7a56fe9bf45053e
FreemanGn <mamoudou999@gmail.com>
2021-02-21 18:13:21 +0100

    Premier commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "07e6bb5628662349693a420b8acb5eb4"
+    }
+}
+
+module.exports = constants;
