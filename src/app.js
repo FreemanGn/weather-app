1118ebf0218c520a8c63fbdfa7aa56d791ffb38a
Freeman_Gn <49251879+FreemanGn@users.noreply.github.com>
2021-02-26 01:39:14 +0100

    test webhook 3

--- src/app.js
+++ src/app.js
@@ -56,5 +56,5 @@ app.get("*", (req, res) => {
 
 
 app.listen(port, () => {
-    console.log("Server is up and running on port: ", port);
+    console.log("Server is up and running on the port: ", port);
 })
